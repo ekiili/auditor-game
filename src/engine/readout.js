7aeb4f130399e43da017053c0209be603171c519
Eero Kiili <eero.kiili@gmail.com>
2026-07-29 16:00:18 +0300

    feat: add accessibility readout module

---
+++ src/engine/readout.js
@@ -0,0 +1,91 @@
+import { computeAccessibleDescription, computeAccessibleName } from 'dom-accessibility-api'
+
+const RELEVANT_ATTRIBUTES = [
+  'alt',
+  'aria-label',
+  'aria-labelledby',
+  'aria-describedby',
+  'title',
+  'type',
+  'id',
+]
+
+function getImplicitRole(element) {
+  const tagName = element.tagName.toLowerCase()
+
+  switch (tagName) {
+    case 'img':
+      return 'img'
+    case 'button':
+      return 'button'
+    case 'a':
+      return element.hasAttribute('href') ? 'link' : null
+    case 'input':
+      if (element.type === 'number') return 'spinbutton'
+      if (element.type === 'text') return 'textbox'
+      return null
+    case 'h1':
+    case 'h2':
+    case 'h3':
+    case 'h4':
+    case 'h5':
+    case 'h6':
+      return 'heading'
+    default:
+      return null
+  }
+}
+
+function getRole(element) {
+  const explicitRole = element.getAttribute('role')
+  return explicitRole || getImplicitRole(element)
+}
+
+function getRelevantAttributes(element) {
+  const attributes = {}
+  for (const name of RELEVANT_ATTRIBUTES) {
+    if (element.hasAttribute(name)) {
+      attributes[name] = element.getAttribute(name)
+    }
+  }
+  return attributes
+}
+
+function roundToOneDecimal(value) {
+  return Math.round(value * 10) / 10
+}
+
+export function inspectElement(element) {
+  const rect = element.getBoundingClientRect()
+  const accessibleName = computeAccessibleName(element)
+  const accessibleDescription = computeAccessibleDescription(element)
+
+  return {
+    tagName: element.tagName.toLowerCase(),
+    role: getRole(element),
+    accessibleName: accessibleName === '' ? null : accessibleName,
+    accessibleDescription: accessibleDescription === '' ? null : accessibleDescription,
+    width: roundToOneDecimal(rect.width),
+    height: roundToOneDecimal(rect.height),
+    attributes: getRelevantAttributes(element),
+  }
+}
+
+export function inspectFocus(element) {
+  const style = getComputedStyle(element)
+  const outlineStyle = style.outlineStyle
+  const outlineWidth = style.outlineWidth
+  const outlineColor = style.outlineColor
+  const boxShadow = style.boxShadow
+
+  const hasOutline = outlineStyle !== 'none' && Number.parseFloat(outlineWidth) > 0
+  const hasBoxShadow = boxShadow !== 'none'
+
+  return {
+    outlineStyle,
+    outlineWidth,
+    outlineColor,
+    boxShadow,
+    hasVisibleIndicator: hasOutline || hasBoxShadow,
+  }
+}
